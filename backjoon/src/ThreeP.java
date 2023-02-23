db5630250a60d54d7e995f6aeedfa8b77c988a05
gitKDH <reg0212@naver.com>
2023-02-23 10:15:30 +0900

    02.23 commit

---
+++ backjoon/src/ThreeP.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ThreeP {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        long a = value.nextLong();
+        long b = value.nextLong();
+        long c = value.nextLong();
+
+        System.out.println(a + b + c);
+
+    }
+}
