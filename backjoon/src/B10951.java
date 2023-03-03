18ce063d98b75b68ecc368e2622d02554bf34935
gitKDH <reg0212@naver.com>
2023-03-03 11:13:35 +0900

    03.03 첫 풀이

---
+++ backjoon/src/B10951.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class B10951 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+
+        while(value.hasNext()){
+            int a = value.nextInt();
+            int b = value.nextInt();
+            System.out.println(a + b);
+        }
+
+
+    }
+}
