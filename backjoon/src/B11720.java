0fe896ea13de272bb9f0968c3e52ce5be0dfa0f2
gitKDH <reg0212@naver.com>
2023-03-06 09:59:48 +0900

    03.06 B11720 수정

--- backjoon/src/B11720.java
+++ backjoon/src/B11720.java
@@ -1,9 +1,17 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class B11720 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader value = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(value.readLine());
+        String[] b = value.readLine().split("");
+        int sum = 0;
+        for(int i = 0; i < a; i++){
+            sum += Integer.parseInt(b[i]);
+        }
+        System.out.println(sum);
 
 
     }
