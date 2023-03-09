08a7094d778da7d19071cd6b3ef0822eb16b9fb9
gitKDH <reg0212@naver.com>
2023-03-09 13:05:12 +0900

    03.09 문제풀이

--- backjoon/src/B2439.java
+++ backjoon/src/B2439.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class B2439 {
     public static void main(String[] args) {
-
+        Scanner value =  new Scanner(System.in);
+        int a = value.nextInt();
+        for(int i = 1; i <= a; i++){
+            for(int j = 1; j <= a-i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <=i; k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
---
+++ backjoon/src/B2742.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class B2742 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+        int a = value.nextInt();
+        for(int i = 0; i < a; i++){
+            System.out.println(a-i);
+        }
+    }
+}
