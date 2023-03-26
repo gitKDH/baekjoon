0dbf83d28c97b35629fb70387cbcc641a761bc20
gitKDH <reg0212@naver.com>
2023-03-26 15:30:41 +0900

    03.17 문제풀이

---
+++ backjoon/src/B2577.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B2577 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+        int a = value.nextInt();
+        int b = value.nextInt();
+        int c = value.nextInt();
+
+        int sum = a * b * c;
+        String result = Integer.toString(sum);
+        System.out.println(result);
+
+        for(int i = 0; i < 10; i++){
+            int count = 0;
+            for(int j = 0; j < result.length(); j++){
+
+            }
+        }
+
+    }
+}
