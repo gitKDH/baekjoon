4c33f35d395a98adaca158c8dacbf891cbf13eac
gitKDH <reg0212@naver.com>
2023-04-10 13:43:21 +0900

    04.10 문제풀이

---
+++ backjoon/src/B1809.java
@@ -0,0 +1,10 @@
+public class B1809 {
+    public static void main(String[] args) {
+        System.out.println("(___)\n" +
+                "(o o)____/\n" +
+                " @@      \\\n" +
+                "  \\ ____,/\n" +
+                "  //   //\n" +
+                " ^^   ^^");
+    }
+}
