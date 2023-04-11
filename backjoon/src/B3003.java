f8c2df0df242aba96092a01467833755307de136
gitKDH <reg0212@naver.com>
2023-04-11 23:56:37 +0900

    04.11 문제풀이

---
+++ backjoon/src/B3003.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class B3003 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        int a = value.nextInt();
+        int b = value.nextInt();
+        int c = value.nextInt();
+        int d = value.nextInt();
+        int e = value.nextInt();
+        int f = value.nextInt();
+
+
+        System.out.print(a-1+" ");
+        System.out.print(b-1+" ");
+        System.out.print(c-2+" ");
+        System.out.print(d-2+" ");
+        System.out.print(e-2+" ");
+        System.out.print(f-8+" ");
+    }
+}
