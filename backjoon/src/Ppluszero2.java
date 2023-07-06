6173f195ef3c27b5775418fa4fe86d9fcf072f0b
gitKDH <reg0212@naver.com>
2023-07-06 18:36:52 +0900

    프로그래머스

---
+++ backjoon/src/Ppluszero2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ppluszero2 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        int a = value.nextInt();
+        int b = value.nextInt();
+        Solution result = new Solution();
+        System.out.println(result.solution(a,b));
+    }
+
+    static class Solution {
+        public int solution(int a, int b) {
+            int answer = 0;
+            String c = Integer.toString(a);
+            String d = Integer.toString(b);
+
+            String word = c + d;
+            String word2 = d + c;
+
+            if(Integer.parseInt(word) < Integer.parseInt(word2)){
+                answer = Integer.parseInt(word2);
+            }else{
+                answer = Integer.parseInt(word);
+            }
+            return answer;
+        }
+    }
+}
