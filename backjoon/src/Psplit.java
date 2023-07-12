9ed51fa0ba00ef1d5947995563b3e8a51589a2c8
gitKDH <reg0212@naver.com>
2023-07-12 16:20:55 +0900

    프로그래머스

--- backjoon/src/Psplit.java
+++ backjoon/src/Psplit.java
@@ -4,6 +4,11 @@ public class Psplit {
     public static void main(String[] args) {
         Scanner value = new Scanner(System.in);
 
-        String a = "";
+        int num1 = value.nextInt();
+        int num2 = value.nextInt();
+
+        float num3 = (float) num1 / (float) num2 * 1000;
+
+        System.out.println(num3);
     }
 }
