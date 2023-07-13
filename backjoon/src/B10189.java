fd982ddcc30b82836ef96fad8baee26d57665bbc
gitKDH <reg0212@naver.com>
2023-07-13 18:19:01 +0900

    백준 새싹

---
+++ backjoon/src/B10189.java
@@ -0,0 +1,8 @@
+public class B10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
