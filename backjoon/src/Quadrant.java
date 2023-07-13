8507bc2ad424c3e9aded6fa912e062c3be89023d
gitKDH <reg0212@naver.com>
2023-07-13 18:15:27 +0900

    프로그래머스

--- backjoon/src/Quadrant.java
+++ backjoon/src/Quadrant.java
@@ -7,13 +7,13 @@ public class Quadrant {
         int y = value.nextInt();
         int num = 0;
 
-        if(x > 0 && y > 0){
+        if (x > 0 && y > 0) {
             num = 1;
         } else if (x < 0 && y > 0) {
             num = 2;
         } else if (x < 0 && y < 0) {
             num = 3;
-        }else num = 4;
+        } else num = 4;
         System.out.println(num);
     }
 }
