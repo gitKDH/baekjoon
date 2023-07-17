ef0aa2e88d6e876e18a24b07ddaade8e7f8893b6
gitKDH <reg0212@naver.com>
2023-07-17 19:12:33 +0900

    프로그래머스 배열

--- backjoon/src/Pstep8.java
+++ backjoon/src/Pstep8.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Pstep8 {
     public static void main(String[] args) {
-
+        Solution result = new Solution();
+        int[] numbers = {1, 2, 3, 4, 5};
+        int[] answer = result.solution(numbers);
+        System.out.println(Arrays.toString(answer));
+    }
+}
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            answer[i] = numbers[i] * 2;
+        }
+        return answer;
     }
 }
