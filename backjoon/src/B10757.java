e2df622d98aa85f6b7c2c4340f418ec8519d60b7
gitKDH <reg0212@naver.com>
2023-07-26 19:44:34 +0900

    백준

---
+++ backjoon/src/B10757.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B10757 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        BigInteger a = value.nextBigInteger();
+        BigInteger b = value.nextBigInteger();
+        //다시
+    }
+}
