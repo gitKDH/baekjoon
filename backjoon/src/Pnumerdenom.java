b6942c6fcdd1f815b59b953207554ef208692f97
gitKDH <reg0212@naver.com>
2023-08-04 00:29:14 +0900

    프로그래머스 다시

---
+++ backjoon/src/Pnumerdenom.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Pnumerdenom {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Scanner value = new Scanner(System.in);
+
+        int a = value.nextInt();
+        int b = value.nextInt();
+        int c = value.nextInt();
+        int d = value.nextInt();
+
+
+        System.out.println(Arrays.toString(solution.solution(a, b, c, d)));
+    }
+    static class Solution {
+        public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
