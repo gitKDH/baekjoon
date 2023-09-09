0b26b066d97f6e964c57a6e35dd3a0677729e224
gitKDH <reg0212@naver.com>
2023-09-09 17:03:49 +0900

    백준 중간

---
+++ backjoon/src/B1264.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B1264 {
+    public static void main(String[] args) {
+
+        Scanner value = new Scanner(System.in);
+
+        boolean n = true;
+
+
+    }
+}
