89585509748c30723f3d63e2f2ece69d9b5170ad
gitKDH <reg0212@naver.com>
2023-09-12 20:34:51 +0900

    백준

--- backjoon/src/B1264.java
+++ backjoon/src/B1264.java
@@ -3,9 +3,31 @@ import java.util.Scanner;
 public class B1264 {
     public static void main(String[] args) {
 
-        Scanner value = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        boolean n = true;
+        for(;;) {
+            String str = sc.nextLine();
+            int count = 0;
+            if(str.equals("#")) {
+                break;
+            }
+
+            str = str.toLowerCase();
+
+            for(int i=0; i<str.length(); i++) {
+                switch(str.charAt(i)) {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        count++;
+                    default:
+                        break;
+                }
+            }
+            System.out.println(count);
+        }
 
 
     }
