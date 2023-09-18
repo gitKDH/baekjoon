fd72ae37285411bdb5bdb0db20e6e00051093527
gitKDH <reg0212@naver.com>
2023-09-18 11:26:16 +0900

    직사각형에서 탈출 - Math함수

--- backjoon/src/B1085.java
+++ backjoon/src/B1085.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class B1085 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int x = in.nextInt();
-        int y = in.nextInt();
-        int w = in.nextInt();
-        int h = in.nextInt();
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int w = input.nextInt();
+        int h = input.nextInt();
 
-        int x_min = Math.min(x, w-x);	// x축 최소거리
-        int y_min = Math.min(y, h-y);	// y축 최소거리
+        int xMin = Math.min(x, w-x);
+        int yMin = Math.min(y, h-y);
 
-        // x와 y축 최소거리 중 가장 작은 값
-        System.out.println(Math.min(x_min, y_min));
+        System.out.println(Math.min(xMin,yMin));
     }
 
 }
