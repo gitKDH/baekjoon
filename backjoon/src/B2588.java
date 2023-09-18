6d957f620dacdd2b391a342558a995f9db48402d
gitKDH <reg0212@naver.com>
2023-09-18 10:54:10 +0900

    곱셈 - 입출력

--- backjoon/src/B2588.java
+++ backjoon/src/B2588.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class B2588 {
     public static void main(String[] args) {
         Scanner value = new Scanner(System.in);
-        int a = value.nextInt();
-        int b = value.nextInt();
 
-        System.out.println(a*(b%10));
-        System.out.println(a*((b%100)/10));
-        System.out.println(a*(b/100));
-        System.out.println(a*b);
+        int A = value.nextInt();
+        int B = value.nextInt();
+
+        System.out.println(A * (B%10));
+        System.out.println(A * ((B%100)/10));
+        System.out.println(A * (B/100));
+        System.out.println(A * B);
     }
 }
