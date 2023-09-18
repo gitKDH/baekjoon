dedb58153b1966c0f6d39c2dea093b14fedad43b
gitKDH <reg0212@naver.com>
2023-09-18 10:53:56 +0900

    사칙연산 - 입출력

---
+++ backjoon/src/B10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class B10869 {
+    public static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+
+        int A = value.nextInt();
+        int B = value.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
