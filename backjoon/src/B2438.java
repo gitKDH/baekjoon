e47d719c94518b422e270e4cacae0fd595208088
gitKDH <reg0212@naver.com>
2023-09-18 11:50:55 +0900

    별찍기(1) - 중첩for

--- backjoon/src/B2438.java
+++ backjoon/src/B2438.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class B2438 {
     public static void main(String[] args) {
-        Scanner value = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int n = value.nextInt();
+        int N = input.nextInt();
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j <= i; j++){
+        for(int x = 0; x < N; x++){
+            for(int y = 0; y<=x; y++){
                 System.out.print("*");
             }
             System.out.println(" ");
