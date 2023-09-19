d47b33c82eee3968e87f1bc5a3ef91ab1b2f9cba
gitKDH <reg0212@naver.com>
2023-09-19 16:00:14 +0900

    달팽이는 올라가고 싶다 - 수학

--- backjoon/src/B2869.java
+++ backjoon/src/B2869.java
@@ -2,18 +2,15 @@ import java.util.Scanner;
 
 public class B2869 {
     public static void main(String[] args) {
-        Scanner value = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int A = value.nextInt();
-        int B = value.nextInt();
-        int V = value.nextInt();
+        double A = input.nextInt();
+        double B = input.nextInt();
+        double V = input.nextInt();
+        double temp = (V-B)/(A-B);
+        double day = Math.ceil(temp);
+        System.out.println((int)day);
 
-        int day = (V - B) / (A - B);
 
-        // 나머지가 있을 경우 (잔여 블럭이 있을 경우)
-        if ((V - B) % (A - B) != 0) {
-            day++;
-        }
-        System.out.println(day);
     }
 }
