676688edda86f3806b6544031de693793a1ec8c9
gitKDH <reg0212@naver.com>
2023-09-19 13:57:38 +0900

    아스키 코드

--- backjoon/src/B11654.java
+++ backjoon/src/B11654.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class B11654 {
     public static void main(String[] args) {
-        Scanner value = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        String a = value.nextLine();
+        String a = input.next();
         char b = a.charAt(0);
 
         System.out.println((int)b);
-
     }
 }
