2caf0720d19159d10c60dcdc0dd35fbc5650cf27
gitKDH <reg0212@naver.com>
2023-09-19 15:28:42 +0900

    상수 - 문자배열 변환

--- backjoon/src/B2908.java
+++ backjoon/src/B2908.java
@@ -1,23 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class B2908 {
     public static void main(String[] args) {
-        Scanner value = new Scanner(System.in);
-        String a = value.next();
-        String b = value.next();
+        Scanner input = new Scanner(System.in);
+        String A = input.next();
+        String B = input.next();
 
-        StringBuffer ar = new StringBuffer(a);
-        StringBuffer br = new StringBuffer(b);
+        char[] Achar = A.toCharArray();
+        char[] Bchar = B.toCharArray();
 
-        String newa = String.valueOf(ar.reverse());
-        String newb = String.valueOf(br.reverse());
+        char Atemp = Achar[0];
+        Achar[0] = Achar[2];
+        Achar[2] = Atemp;
 
-        int ina = Integer.parseInt(newa);
-        int inb = Integer.parseInt(newb);
+        char Btemp = Bchar[0];
+        Bchar[0] = Bchar[2];
+        Bchar[2] = Btemp;
 
-        if(ina > inb){
-            System.out.println(ina);
-        }else System.out.println(inb);
+        A = new String(Achar);
+        B = new String(Bchar);
+
+        int resultA = Integer.parseInt(A);
+        int resultB = Integer.parseInt(B);
+
+        System.out.println(Math.max(resultA,resultB));
 
     }
 }
