5f4eecc3518940d4e137108bff14795344f6ffd9
gitKDH <reg0212@naver.com>
2023-09-19 09:28:55 +0900

    최댓값 - 배열 for문

--- backjoon/src/B2562.java
+++ backjoon/src/B2562.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class B2562 {
     public static void main(String[] args) {
-        Scanner value = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        int[] a = new int[9];
+        int[] index = new int[9];
         int max = 0;
-        int index = 0;
+        int n = 0;
 
-        for(int i = 0; i < 9; i++){
-            int b = value.nextInt();
-            a[i] = b;
+        for(int i = 0; i<index.length; i++){
+            index[i] = input.nextInt();
         }
-        for(int i = 0; i < 9; i++){
-            if(a[i] > max){
-                max = a[i];
-                index = i+1;
+        for(int i = 0; i<=8; i++){
+            if(index[i]>max){
+                max = index[i];
+                n = i;
             }
         }
         System.out.println(max);
-        System.out.println(index);
+        System.out.println(n+1);
     }
 }
