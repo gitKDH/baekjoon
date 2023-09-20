a133d908a580c1170b8b217126844ff3ceab5de5
gitKDH <reg0212@naver.com>
2023-09-20 11:41:51 +0900

    소수찾기 - for, if

--- backjoon/src/B1978.java
+++ backjoon/src/B1978.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 
 public class B1978 {
     public static void main(String[] args) {
-        Scanner value = new Scanner(System.in);
-        int n = value.nextInt();
-        int sum = 0;
-        int pn;
-        for (int i = 0; i < n; i++) {
-            pn = value.nextInt();
-            for (int p = 2; p <= pn; p++) {
-                if (p == pn) {
-                    sum += 1;
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int isPrime = 0;
+        int count = 0;
+
+        for(int i = 0; i < N; i++){
+            isPrime = input.nextInt();
+            for(int j = 2; j <= isPrime; j++){
+                if(j == isPrime){
+                    count++;
                 }
-                if (pn % p == 0) {
+                if(isPrime % j == 0){
                     break;
                 }
             }
         }
-        System.out.println(sum);
+        System.out.println(count);
     }
 }
