aa5520237a28d1cad27d135318a0155b44454081
gitKDH <reg0212@naver.com>
2023-09-27 17:39:52 +0900

    숫자의 합

--- baekjoon/src/B11720.java
+++ baekjoon/src/B11720.java
@@ -4,15 +4,14 @@ import java.io.InputStreamReader;
 
 public class B11720 {
     public static void main(String[] args) throws IOException {
-        BufferedReader value = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(value.readLine());
-        String[] b = value.readLine().split("");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(input.readLine());
         int sum = 0;
-        for(int i = 0; i < a; i++){
-            sum += Integer.parseInt(b[i]);
+        String[] x = input.readLine().split("");
+        for(int i = 0; i < n; i++){
+            sum += Integer.parseInt(x[i]);
         }
         System.out.println(sum);
-
-
     }
 }
