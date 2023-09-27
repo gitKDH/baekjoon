fca0d4d2a73bd5080d73cc51e2505abb2f50d5d7
gitKDH <reg0212@naver.com>
2023-09-27 18:19:34 +0900

    대칭 차집합 - 실패

---
+++ baekjoon/src/B1269.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class B1269 {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        int an = input.nextInt();
+        int bn = input.nextInt();
+
+        int[] a = new int[an];
+        int[] b = new int[bn];
+
+        for(int i = 0; i < an; i++){
+            a[i] = input.nextInt();
+        }
+        for(int i = 0; i < bn; i++){
+            b[i] = input.nextInt();
+        }
+        for(int i = 0; i < an; i++){
+
+        }
+
+    }
+}
